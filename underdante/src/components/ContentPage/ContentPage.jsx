a042760edcf038f180e34e5a714ea11391b60467
Petr <petemashine@gmail.com>
2022-10-12 22:20:37 +0300

    asyncThunk add

--- underdante/src/components/ContentPage/ContentPage.jsx
+++ underdante/src/components/ContentPage/ContentPage.jsx
@@ -1,38 +1,37 @@
-import React ,{useEffect}  from "react";
-import { useSelector , useDispatch } from "react-redux";
+import React from "react";
+// import { useSelector , useDispatch } from "react-redux";
 import CurrentPage from "./CurrentPage";
-import paths from "../../paths/paths";
-import { setFullData } from "../../redux/slices/fullDataSlice";
+// import paths from "../../paths/paths";
 
 export default function ContentPage () {
 
-const pagesAmount = useSelector(state=>state.pagesAmount) ;
-const dispatch = useDispatch() ;
+// const pagesAmount = useSelector(state=>state.pagesAmount) ;
+// const dispatch = useDispatch() ;
 
-useEffect(()=>{
-async function getAllUsers() {
-    console.log(pagesAmount) ;
-    if (pagesAmount.isDone) {
-    let resultArr = [] ;
+// useEffect(()=>{
+// async function getAllUsers() {
+//     console.log(pagesAmount) ;
+//     if (pagesAmount.isDone) {
+//     let resultArr = [] ;
 
-    for(let i = 1 ; i <= pagesAmount.amount ; i++) {
-        let responce = await fetch(paths.getUsers(i)) ;
-        let json = await responce.json() ;
+//     for(let i = 1 ; i <= pagesAmount.amount ; i++) {
+//         let responce = await fetch(paths.getUsers(i)) ;
+//         let json = await responce.json() ;
 
-        resultArr.push(json) ;
+//         resultArr.push(json) ;
 
-    }
-    let res = Promise.all(resultArr) ;
-    let array = await res ;
-    dispatch(setFullData({data:array})) ;
-    console.log("getallusers") ;
-    resultArr = null ;
-    }
-}
-getAllUsers() ;
+//     }
+//     let res = Promise.all(resultArr) ;
+//     let array = await res ;
+//     dispatch(setFullData({data:array})) ;
+//     console.log("getallusers") ;
+//     resultArr = null ;
+//     }
+// }
+// getAllUsers() ;
 
 
-} , [pagesAmount]) ;
+// } , [pagesAmount]) ;
 
 
 
--- underdante/src/redux/store/store.js
+++ underdante/src/redux/store/store.js
@@ -1,15 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit' ;
-import pagesAmountSlice from '../slices/pagesAmountSlice' ;
-import currentDataSlice  from '../slices/currentDataSlice';
-import fullDataSlice from '../slices/fullDataSlice';
 import dataSlice from '../slices/dataSlice';
 
 
 export default configureStore({
   reducer: {
-    pagesAmount : pagesAmountSlice ,
-    currentData : currentDataSlice ,
-    fullData:fullDataSlice ,
     data : dataSlice ,
   },
 });
