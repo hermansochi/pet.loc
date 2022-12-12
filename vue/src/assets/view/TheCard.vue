f8829d3821cd86562ec02506097c5b8a70235ce7
cleo412 <semenenko412@gmail.com>
2022-12-12 22:58:30 +0300

    cleo412:Front:Desabled input in TheCard

--- vue/src/assets/view/TheCard.vue
+++ vue/src/assets/view/TheCard.vue
@@ -4,90 +4,158 @@ const EmplStore = useEmplStore();
 </script>
 
 <template>
-  <div></div>
-  <form class="container_form" @submit.prevent>
+  <form class="container_form">
     <fieldset class="container container_fullname">
       <div class="photo" width="128" height="128"></div>
 
       <div class="wrapper">
         <label for="middle_name">SURNAME:</label>
-        <input id="middle_name" name="middle_name" :value="empl.last_name" />
+        <input
+          id="middle_name"
+          name="middle_name"
+          :value="empl.last_name"
+          :disabled="disabled"
+        />
       </div>
 
       <div class="wrapper">
         <label for="first_name">NAME:</label>
-        <input id="first_name" name="first_name" :value="empl.first_name" />
+        <input
+          id="first_name"
+          name="first_name"
+          :value="empl.first_name"
+          :disabled="disabled"
+        />
       </div>
 
       <div class="wrapper">
         <label for="last_name">PATRONYMIC :</label>
-        <input id="last_name" name="last_name" :value="empl.middle_name" />
+        <input
+          id="last_name"
+          name="last_name"
+          :value="empl.middle_name"
+          :disabled="disabled"
+        />
       </div>
     </fieldset>
 
     <fieldset class="container container_contacts">
       <div class="wrapper">
         <label for="id_employee">ID:</label>
-        <input id="id_employee" name="id_employee" :value="paramsId" disabled />
-      </div>
-
-      <div class="wrapper">
-        <label for="born">BIRTH:</label>
-        <input id="born" name="born" :value="empl.birthday" />
-      </div>
-
-      <div class="wrapper">
-        <label for="gender">GENDER:</label>
-        <input id="gender" name="gender" :value="getEmplGender" />
-      </div>
-
-      <div class="wrapper wrapper_email">
-        <label for="email">EMAIL:</label>
-        <input id="email" name="email" :value="empl.email" />
+        <input
+          id="id_employee"
+          name="id_employee"
+          :value="paramsId"
+          :disabled="true"
+        />
       </div>
 
       <div class="wrapper wrapper_phone">
         <label for="phone">PHONE:</label>
-        <input id="phone" name="phone" :value="empl.telephone" />
+        <input
+          id="phone"
+          name="phone"
+          :value="empl.telephone"
+          :disabled="disabled"
+        />
+      </div>
+
+      <div class="wrapper">
+        <label for="gender">GENDER:</label>
+        <select id="gender" :disabled="disabled">
+          <option selected>{{ getEmplGender }}</option>
+          <option>male</option>
+          <option>female</option>
+          <option>unknown</option>
+        </select>
+      </div>
+
+      <div class="wrapper wrapper_email">
+        <label for="email">EMAIL:</label>
+        <input
+          id="email"
+          name="email"
+          :value="empl.email"
+          :disabled="disabled"
+        />
+      </div>
+
+      <div class="wrapper">
+        <label for="born">BIRTH:</label>
+        <input
+          id="born"
+          name="born"
+          :value="empl.birthday"
+          :disabled="disabled"
+        />
       </div>
 
       <div class="wrapper" wrapper_mobile>
         <label for="mobile">MOBILE:</label>
-        <input id="mobile" name="mobile" :value="empl.mobile" />
+        <input
+          id="mobile"
+          name="mobile"
+          :value="empl.mobile"
+          :disabled="disabled"
+        />
       </div>
     </fieldset>
 
     <fieldset class="container">
       <div class="wrapper">
         <label for="company">CITY:</label>
-        <input id="city" name="city" :value="empl.city" />
+        <input id="city" name="city" :value="empl.city" :disabled="disabled" />
       </div>
 
       <div class="wrapper">
         <label for="company">COMPANY:</label>
-        <input id="company" name="company" :value="empl.company" />
+        <input
+          id="company"
+          name="company"
+          :value="empl.company"
+          :disabled="disabled"
+        />
       </div>
 
       <div class="wrapper">
         <label for="department">DEPATMENT:</label>
-        <input id="department" name="department" :value="empl.department" />
+        <input
+          id="department"
+          name="department"
+          :value="empl.department"
+          :disabled="disabled"
+        />
       </div>
 
       <div class="wrapper">
         <label for="position">POSITION:</label>
-        <input id="position" type="text" name="position" :value="empl.title" />
+        <input
+          id="position"
+          type="text"
+          name="position"
+          :value="empl.title"
+          :disabled="disabled"
+        />
       </div>
     </fieldset>
 
     <fieldset class="container">
-      <textarea placeholder="about..."></textarea>
+      <textarea rows="6" placeholder="about..." :disabled="disabled"></textarea>
     </fieldset>
+    <button type="button" class="button btn_edit" @click="disabled = !disabled">
+      {{ disabled ? "Edit employee" : "Save employee" }}
+    </button>
   </form>
 </template>
 
 <script>
 export default {
-  isEditing: true,
+  data() {
+    return {
+      disabled: true,
+    };
+  },
+
   created() {
     // Id from the router (RouterLink in TheElem)
     this.paramsId = this.$route.params.id;
@@ -113,9 +181,8 @@ export default {
 <style scoped>
 .container_form {
   display: grid;
-  grid-template-columns: 128px ifr 1fr;
   gap: 20px;
-  max-width: 80%;
+  width: 80%;
   background-color: var(--vt-c-white-mute);
   padding: 40px;
   margin: 0 auto;
@@ -126,30 +193,27 @@ export default {
   border: none;
   gap: 10px;
 }
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 88px auto;
+  align-content: center;
+}
+
 .container_fullname {
-  grid-template-columns: 128px auto;
+  grid-template-columns: 148px auto;
 }
 
 .photo {
   grid-row: 1/4;
   background-color: var(--vt-c-white-background);
   border-radius: 20%;
-  padding: 20px;
+  margin-right: 20px;
 }
 
 .container_contacts {
-  grid-template-columns: auto 1fr;
-}
-
-.container_contacts .wrapper:first-child,
-.container_contacts .wrapper:nth-child(3),
-.container_contacts .wrapper:nth-child(5) {
-  margin-right: 60px;
-}
-
-label {
   display: grid;
-  align-content: center;
+  grid-template-columns: 280px 1fr;
 }
 
 select {
@@ -157,13 +221,12 @@ select {
 }
 
 textarea {
-  min-height: 60px;
   resize: none;
   white-space: pre-line;
 }
 
-.wrapper {
-  display: grid;
-  grid-template-columns: 90px 1fr;
+.btn_edit {
+  justify-self: end;
+  margin: 10px;
 }
 </style>
